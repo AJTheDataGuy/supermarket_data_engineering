b4643e27f2b850252719d7d1fc5b94a5bdef3471
AJTheDataGuy <TheDudeFish2@gmail.com>
2023-07-29 21:47:26 +0800

    fixed bug with sqlalchemy data types when uploading to postgresql

--- 02. etl_mongodb_to_postgres/test_if_schema_change_vs_expected.py
+++ 02. etl_mongodb_to_postgres/test_if_schema_change_vs_expected.py
@@ -24,30 +24,13 @@ def main():
     """main"""
     mongodb_collection = connect_to_mongodb()
     json_data_list = mongodb_collection.find({})
-
     flattened_json_df = pd.json_normalize(json_data_list)
     flattened_json_df_w_specials = add_specials_columns_if_not_present(
         flattened_json_df
     )
-
     schema_has_changed_bool = test_if_schema_has_changed(flattened_json_df_w_specials)
     if schema_has_changed_bool:
         raise ValueError("Schema has changed from expected. Check the errors log.")
-
-    cleaned_data_types_df = change_data_types_for_problem_columns(
-        flattened_json_df_w_specials
-    )
-    sql_data_type_definitions_dict = define_column_data_types_for_sql()
-    postgres_connection = connect_to_postgresql()
-    cleaned_data_types_df.to_sql(
-        name="raw_from_mongodb_test",
-        con=postgres_connection,
-        schema="public",
-        if_exists="replace",
-        index=False,
-        method="multi",
-        dtype=sql_data_type_definitions_dict,
-    )
 
 
 def connect_to_mongodb(
@@ -99,57 +82,6 @@ def connect_to_postgresql(
     return engine
 
 
-def define_column_data_types_for_sql() -> dict:
-    """This overall script uses the pd.to_sql function to insert data into PostgreSQL
-    which infers column types unless otherwise defined
-
-    Numeric columns should be defined as numeric before being inserted into Postgres
-    Other columns are OK to be text.
-
-    If mixed data is present - such as a decimal and a text value in the same column
-    an error should be raised before being inserted so that can be corrected
-
-    Dictionary is provided in pairs of {"column":"data type"}
-
-    Text columns have been left out of this function for now. If space was a
-    requirement in the database, the text column datatypes could be made smaller,
-    for example going from text --> VARCHAR(25).
-
-    Also in the future this function could be moved to a config file
-
-    Parameters: None
-
-    Returns:
-    1. data_types_definitions_dict: dictionary of how certain (but not all)
-    columns present in the MongoDB JSON data should be represented in the PostgreSQL data
-    """
-    sqlalchemy_int = sqlalchemy.types.INTEGER()
-    sqlalchemy_bool = sqlalchemy.types.BOOLEAN()
-    sqlalchemy_numeric = sqlalchemy.types.NUMERIC()
-
-    data_type_definitions_dict = {
-        "id": sqlalchemy_int,
-        "date_extracted": sqlalchemy.types.DATE(),
-        "featured": sqlalchemy_bool,
-        "restrictions.retailLimit": sqlalchemy_int,
-        "restrictions.promotionalLimit": sqlalchemy_int,
-        "restrictions.liquorAgeRestrictionFlag": sqlalchemy_bool,
-        "restrictions.tobaccoAgeRestrictionFlag": sqlalchemy_bool,
-        "restrictions.restrictedByOrganisation": sqlalchemy_bool,
-        "pricing.now": sqlalchemy_numeric,
-        "pricing.was": sqlalchemy_numeric,
-        "pricing.unit.quantity": sqlalchemy_int,
-        "pricing.unit.ofMeasureQuantity": sqlalchemy_int,
-        "pricing.unit.price": sqlalchemy_numeric,
-        "pricing.unit.isWeighted": sqlalchemy_bool,
-        "pricing.onlineSpecial": sqlalchemy_bool,
-        "pricing.multiBuyPromotion.id": sqlalchemy_int,
-        "pricing.multiBuyPromotion.minQuantity": sqlalchemy_int,
-        "pricing.multiBuyPromotion.reward": sqlalchemy_numeric,
-    }
-    return data_type_definitions_dict
-
-
 def add_specials_columns_if_not_present(
     raw_mongodb_table: pd.DataFrame,
 ) -> pd.DataFrame:
@@ -169,10 +101,8 @@ def add_specials_columns_if_not_present(
         "internalDescription",
         "pricing.specialType",
         "pricing.offerDescription",
-        "pricing.multiBuyPromotion.type",
-        "pricing.multiBuyPromotion.id",
-        "pricing.multiBuyPromotion.minQuantity",
-        "pricing.multiBuyPromotion.reward",
+        "pricing.multiBuyPromotion.type" "pricing.multiBuyPromotion.id",
+        "pricing.multiBuyPromotion.minQuantity" "pricing.multiBuyPromotion.reward",
         "pricing.promotionDescription",
     }
 
@@ -361,38 +291,6 @@ def write_errors_file(flag: str):
     3. Data type errors when inserting into PostgreSQL
     """
     raise NotImplementedError(message)
-
-
-def change_data_types_for_problem_columns(raw_dataframe: pd.DataFrame):
-    """Changes the data types of problem columns
-    so the overall dataframe can be ready correctly
-    by the pandas to_sql function with sqlalchemy.
-
-    For example, it seems that the MongoDB data type of ObjectID
-    is retained when converting a dataframe and causes an error
-
-    As another example, sqlalchemy tries to read some columns as dictionaries
-    so they need to be explicitly converted to string before
-    sqlalchemy will accept them and load the dataframe into PostgreSQL.
-
-    Parameters:
-    1. raw_dataframe: raw dataframe containing columns with problematic data types
-
-    Returns:
-    1. type_cleaned_dataframe: dataframe with problematic column data types converted
-        explicitly to something that pandas and sqlalchemy can read and upload into PostgreSQL
-    """
-    type_cleaned_dataframe = raw_dataframe.astype(
-        {
-            "_id": str,
-            "_type": str,
-            "imageUris": str,
-            "locations": str,
-            "onlineHeirs": str,
-            "restrictions.delivery": str,
-        }
-    )
-    return type_cleaned_dataframe
 
 
 if __name__ == "__main__":
